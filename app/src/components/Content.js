f82f69cbf2e2bbe561d0d59af22c6b38e39dcedd
g1eb <mail@g1eb.com>
2021-08-26 16:54:37 -0700

    Move everything header related to a seprate header component

--- app/src/components/Content.js
+++ app/src/components/Content.js
@@ -1,33 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
 
 
 import Data from './Data'
-import Logo from './Logo'
+import Header from './Header'
 import ArrowUp from './ArrowUp'
 import fetchData from '../utils/fetchData'
 
 
-const useStyles = makeStyles(theme => ({
-  header: {
-    color: '#fefefe',
-    marginTop: theme.spacing(3),
-  },
-  logo: {
-    display: 'inline-block',
-    verticalAlign: 'middle',
-    width: theme.spacing(12),
-    height: theme.spacing(12),
-    marginRight: theme.spacing(2),
-  },
-}))
-
-
 const Content = () => {
-
-  const classes = useStyles()
 
   const [data, setData] = useState()
 
@@ -42,14 +23,7 @@ const Content = () => {
   return (
     <Container maxWidth="xl">
       <div id="top" />
-      <Typography component="h3" variant="h3" className={classes.header}>
-        <a href="https://github.com/usc-isi-i2/kgtk" title="Knowledge Graph Toolkit" rel="noopener noreferrer nofollow" target="_blank">
-          <div className={ classes.logo }>
-            <Logo/>
-          </div>
-        </a>
-        KGTK Browser
-      </Typography>
+      <Header />
       {!!data && <Data data={data} />}
       <ArrowUp/>
     </Container>
