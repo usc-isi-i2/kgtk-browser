85535abf35aa2f1b5bbb010e4f45755386fda328
g1eb <mail@g1eb.com>
2021-08-26 15:05:14 -0700

    Render lists of data properties and identifiers

--- app/src/components/Data.js
+++ app/src/components/Data.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
@@ -8,7 +9,7 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(3),
-    paddingTop: theme.spacing(6),
+    paddingTop: theme.spacing(3),
     paddingLeft: theme.spacing(4),
     paddingRight: theme.spacing(4),
     paddingBottom: theme.spacing(2),
@@ -17,6 +18,43 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'start',
     position: 'relative',
+    color: '#fefefe',
+  },
+  link: {
+    display: 'inline-block',
+    padding: theme.spacing(1),
+    color: '#fefefe',
+    transition: '0.2s background ease',
+    '&:hover': {
+      textDecoration: 'underline',
+      cursor: 'pointer',
+    },
+  },
+  link2: {
+    display: 'inline-block',
+    paddingLeft: theme.spacing(1),
+    color: '#fefefe',
+    transition: '0.2s background ease',
+    '&:hover': {
+      textDecoration: 'underline',
+      cursor: 'pointer',
+    },
+  },
+  text: {
+    display: 'inline-block',
+    padding: theme.spacing(1),
+    color: '#fefefe',
+    width: '100%',
+  },
+  text2: {
+    display: 'inline-block',
+    paddingLeft: theme.spacing(1),
+    color: '#fefefe',
+    width: '100%',
+  },
+  row: {
+    paddingTop: theme.spacing(3),
+    borderBottom: '1px solid rgba(255, 255, 255, 0.15)',
   },
 }))
 
@@ -41,6 +79,67 @@ const Data = ({ data }) => {
     return (
       <Grid item xs={12}>
         <Paper className={classes.paper}>
+          {data.properties.map((property, index) => (
+            <Grid container spacing={3} key={index}
+              className={classes.row}>
+              <Grid item xs={3}>
+                <Link variant='body1'
+                  className={classes.link}
+                  href={`/?id=${property.ref}`}>
+                  {property.property}
+                </Link>
+              </Grid>
+              <Grid item xs={9}>
+                {!!property.values && property.values.map((value, index) => (
+                  <Grid container spacing={3} key={index}>
+                    <Grid item xs={12}>
+                      {value.url || value.ref ? (
+                        <Link variant='body1'
+                          className={classes.link}
+                          href={`/?id=${value.ref}`}>
+                          {value.text}
+                        </Link>
+                      ) : (
+                        <Typography variant='body1'
+                          className={classes.text}>
+                          {value.text}
+                        </Typography>
+                      )}
+                      {!!value.qualifiers && value.qualifiers.map((qualifier, index) => (
+                        <Grid container spacing={3} key={index}>
+                          <Grid item xs={4}>
+                            <Link variant='body2'
+                              className={classes.link2}
+                              href={`/?id=${qualifier.ref}`}>
+                              {qualifier.property}
+                            </Link>
+                          </Grid>
+                          <Grid item xs={8}>
+                            {!!qualifier.values && qualifier.values.map((value, index) => (
+                              <Grid container key={index}>
+                                {value.url || value.ref ? (
+                                  <Link variant='body2'
+                                    className={classes.link2}
+                                    href={value.url ? value.url : `/?id=${value.ref}`}>
+                                    {value.text}
+                                  </Link>
+                                ) : (
+                                  <Typography variant='body2'
+                                    className={classes.text2}>
+                                    {value.text}
+                                  </Typography>
+                                )}
+                              </Grid>
+                            ))}
+                          </Grid>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          ))}
         </Paper>
       </Grid>
     )
@@ -59,6 +158,67 @@ const Data = ({ data }) => {
     return (
       <Grid item xs={12}>
         <Paper className={classes.paper}>
+          {data.xrefs.map((property, index) => (
+            <Grid container spacing={3} key={index}
+              className={classes.row}>
+              <Grid item xs={6}>
+                <Link variant='body1'
+                  className={classes.link}
+                  href={`/?id=${property.ref}`}>
+                  {property.property}
+                </Link>
+              </Grid>
+              <Grid item xs={6}>
+                {!!property.values && property.values.map((value, index) => (
+                  <Grid container spacing={3} key={index}>
+                    <Grid item xs={12}>
+                      {value.url || value.ref ? (
+                        <Link variant='body1'
+                          className={classes.link}
+                          href={`/?id=${value.ref}`}>
+                          {value.text}
+                        </Link>
+                      ) : (
+                        <Typography variant='body1'
+                          className={classes.text}>
+                          {value.text}
+                        </Typography>
+                      )}
+                      {!!value.qualifiers && value.qualifiers.map((qualifier, index) => (
+                        <Grid container spacing={3} key={index}>
+                          <Grid item xs={4}>
+                            <Link variant='body2'
+                              className={classes.link2}
+                              href={`/?id=${qualifier.ref}`}>
+                              {qualifier.property}
+                            </Link>
+                          </Grid>
+                          <Grid item xs={8}>
+                            {!!qualifier.values && qualifier.values.map((value, index) => (
+                              <Grid container key={index}>
+                                {value.url || value.ref ? (
+                                  <Link variant='body2'
+                                    className={classes.link2}
+                                    href={value.url ? value.url : `/?id=${value.ref}`}>
+                                    {value.text}
+                                  </Link>
+                                ) : (
+                                  <Typography variant='body2'
+                                    className={classes.text2}>
+                                    {value.text}
+                                  </Typography>
+                                )}
+                              </Grid>
+                            ))}
+                          </Grid>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          ))}
         </Paper>
       </Grid>
     )
