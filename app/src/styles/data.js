c4e2e6a102296fd5b209445ecab7eb0568269c4e
g1eb <mail@g1eb.com>
2021-09-24 14:23:00 -0700

    Update data component styles to have darker text on top of lighter background

--- app/src/styles/data.js
+++ app/src/styles/data.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { alpha, makeStyles } from '@material-ui/core/styles'
 
 
 const useStyles = makeStyles(theme => ({
@@ -7,19 +7,18 @@ const useStyles = makeStyles(theme => ({
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
     paddingBottom: theme.spacing(1),
-    backgroundColor: 'rgba(254, 254, 254, 0.25)',
     borderRadius: 0,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'start',
     position: 'relative',
-    color: '#fefefe',
+    color: '#333',
   },
   link: {
     display: 'inline-block',
     padding: theme.spacing(0),
     textDecoration: 'underline',
-    color: '#fefefe',
+    color: '#333',
     cursor: 'pointer',
     overflow: 'hidden',
     textOverflow: 'ellipsis',
@@ -27,13 +26,14 @@ const useStyles = makeStyles(theme => ({
     width: '100%',
     transition: '0.2s background ease',
     '&:hover': {
-      background: 'rgba(255, 255, 255, 0.1)',
+      background: alpha(theme.palette.common.black, 0.05),
+      color: '#111',
     },
   },
   link2: {
     display: 'inline-block',
     paddingLeft: theme.spacing(3),
-    color: '#fefefe',
+    color: '#333',
     textDecoration: 'underline',
     cursor: 'pointer',
     overflow: 'hidden',
@@ -42,14 +42,15 @@ const useStyles = makeStyles(theme => ({
     width: '100%',
     transition: '0.2s background ease',
     '&:hover': {
-      background: 'rgba(255, 255, 255, 0.1)',
+      background: alpha(theme.palette.common.black, 0.05),
+      color: '#111',
     },
   },
   link3: {
     display: 'inline-block',
     padding: theme.spacing(0),
     textDecoration: 'underline',
-    color: '#fefefe',
+    color: '#333',
     cursor: 'pointer',
     overflow: 'hidden',
     textOverflow: 'ellipsis',
@@ -58,14 +59,15 @@ const useStyles = makeStyles(theme => ({
     transition: '0.2s background ease',
     fontSize: 10,
     '&:hover': {
-      background: 'rgba(255, 255, 255, 0.1)',
+      background: alpha(theme.palette.common.black, 0.05),
+      color: '#111',
     },
   },
   link4: {
     display: 'inline-block',
     padding: theme.spacing(0),
     textDecoration: 'underline',
-    color: '#fefefe',
+    color: '#333',
     cursor: 'pointer',
     overflow: 'hidden',
     textOverflow: 'ellipsis',
@@ -73,36 +75,37 @@ const useStyles = makeStyles(theme => ({
     verticalAlign: 'bottom',
     transition: '0.2s background ease',
     '&:hover': {
-      background: 'rgba(255, 255, 255, 0.1)',
+      background: alpha(theme.palette.common.black, 0.05),
+      color: '#111',
     },
   },
   text: {
     display: 'inline-block',
     padding: theme.spacing(0),
-    color: '#fefefe',
+    color: '#333',
     width: '100%',
   },
   text2: {
     display: 'inline-block',
     padding: theme.spacing(0),
     paddingLeft: theme.spacing(3),
-    color: '#fefefe',
+    color: '#333',
     width: '100%',
   },
   text3: {
     display: 'inline-block',
     padding: theme.spacing(0),
-    color: '#fefefe',
+    color: '#333',
     width: '100%',
     fontSize: 10,
   },
   text4: {
     display: 'inline-block',
     padding: theme.spacing(0),
-    color: '#fefefe',
+    color: '#333',
   },
   lang: {
-    color: '#d4d4d4',
+    color: '#777',
     marginLeft: theme.spacing(1),
   },
   row: {
@@ -121,7 +124,7 @@ const useStyles = makeStyles(theme => ({
     height: '300px',
   },
   title: {
-    color: '#fefefe',
+    color: '#333',
   },
   titleBar: {
     background:
