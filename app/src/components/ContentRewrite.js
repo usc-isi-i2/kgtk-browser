c6819a7792f69617856312a8acb410af1ac8c756
g1eb <mail@g1eb.com>
2021-09-24 13:47:55 -0700

    Reformat content rewrite component to be consistent

--- app/src/components/ContentRewrite.js
+++ app/src/components/ContentRewrite.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import {
+  Route,
+  Switch,
+  BrowserRouter,
+} from 'react-router-dom'
+
 import Content from './Content'
 
 
-//	        <Redirect path='/item:id' to='/?id=:id'/>
 const ContentRewrite = () => (
-	<BrowserRouter>
-	    <Switch>
-	        <Route exact path='/' component={Content}/>
-	        <Route exact path='/browser' component={Content}/>
-	    </Switch>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Content}/>
+      <Route exact path='/browser' component={Content}/>
+    </Switch>
+  </BrowserRouter>
 )
 
 
