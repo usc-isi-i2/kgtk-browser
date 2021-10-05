10ebfabc0070bfbc1e815b4430bf5a9bc6ff0fe4
Craig Milo Rogers <rogers@isi.edu>
2021-10-05 15:12:22 -0700

    Switch to new input file name.  Detect duplicate ID values and provide samples.

--- examples/Arnold_Schwarzenegger/PREPARE_DATA_FOR_BROWSER.sh
+++ examples/Arnold_Schwarzenegger/PREPARE_DATA_FOR_BROWSER.sh
@@ -2,7 +2,7 @@
 
 # Define some locations and options:
 
-export INPUT_FILE="./data/Q2685.graph.all.tsv.gz"
+export INPUT_FILE="./data/arnold.all.tsv.gz"
 export WORKING_FOLDER="./temp"
 export GRAPHS="./graphs"
 
@@ -30,6 +30,13 @@ mkdir -p ${GRAPHS}
 mkdir -p ${SORT_TEMP_FOLDER}
 
 # ******************************************************************
+echo -e "\n*** Remove unwanted columns from the input file. ***"
+time kgtk ${KGTK_OPTIONS} reorder-columns ${MGZIP_OPTIONS} \
+     -i ${INPUT_FILE} \
+     --columns node1 label node2 id --trim \
+     -o ${WORKING_FOLDER}/all.trimmed.tsv.gz
+
+# ******************************************************************
 # Split the unified input file into:
 #    aliases
 #    descriptions
@@ -40,7 +47,7 @@ mkdir -p ${SORT_TEMP_FOLDER}
 # Also split out "link" records, they shouldn't be there.
 echo -e "\n*** Split the unified input file into parts. ***"
 time kgtk ${KGTK_OPTIONS} filter ${MGZIP_OPTIONS} \
-     -i ${INPUT_FILE} \
+     -i ${WORKING_FOLDER}/all.trimmed.tsv.gz \
      -p ';alias;' \
      -o ${WORKING_FOLDER}/aliases.tsv.gz \
      -p ';description;' \
@@ -104,6 +111,19 @@ time kgtk ${KGTK_OPTIONS} deduplicate ${MGZIP_OPTIONS} \
      -i ${WORKING_FOLDER}/aliases.sorted.tsv.gz \
      -o ${GRAPHS}/aliases.tsv.gz
  
+echo -e "\n*** Ensure that the aliases do not contain ID duplicates. ***"
+time kgtk ${KGTK_OPTIONS} unique ${MGZIP_OPTIONS} \
+     --column id --min-count 2 --verbose \
+     -i ${GRAPHS}/aliases.tsv.gz \
+     -o ${WORKING_FOLDER}/aliases.duplicate-ids.tsv.gz \
+
+# Present a sample of the records with duplicate ID values:
+time kgtk ${KGTK_OPTIONS} head ${MGZIP_OPTIONS} -n 5 \
+     -i ${WORKING_FOLDER}/aliases.duplicate-ids.tsv.gz \
+    / ifexists \
+     -i ${GRAPHS}/aliases.tsv.gz --input-keys id \
+     --filter-on - --filter-keys node1
+ 
 echo -e "\n*** Ensure that the descriptions do not contain gross duplicates: sorting... ***"
 time kgtk ${KGTK_OPTIONS} sort ${GZIP_CMD} ${MGZIP_OPTIONS} \
      -X "--parallel ${SORT_PARALLEL} --buffer-size ${SORT_BUFFER_SIZE} -T ${SORT_TEMP_FOLDER}" \
@@ -113,11 +133,24 @@ time kgtk ${KGTK_OPTIONS} sort ${GZIP_CMD} ${MGZIP_OPTIONS} \
 
 echo -e "\n*** Ensure that the descriptions do not contain gross duplicates: deduplicating... ***"
 time kgtk ${KGTK_OPTIONS} deduplicate ${MGZIP_OPTIONS} \
-                      --presorted --report-lists --verbose \
-                      --columns node1 label node2 \
-                      --keep-first id \
+     --presorted --report-lists --verbose \
+     --columns node1 label node2 \
+     --keep-first id \
      -i ${WORKING_FOLDER}/descriptions.sorted.tsv.gz \
      -o ${GRAPHS}/descriptions.tsv.gz
+ 
+echo -e "\n*** Ensure that the descriptions do not contain ID duplicates. ***"
+time kgtk ${KGTK_OPTIONS} unique ${MGZIP_OPTIONS} \
+     --column id --min-count 2 --verbose \
+     -i ${GRAPHS}/descriptions.tsv.gz \
+     -o ${WORKING_FOLDER}/descriptions.duplicate-ids.tsv.gz \
+ 
+# Present a sample of the records with duplicate ID values:
+time kgtk ${KGTK_OPTIONS} head ${MGZIP_OPTIONS} -n 5 \
+     -i ${WORKING_FOLDER}/descriptions.duplicate-ids.tsv.gz \
+    / ifexists \
+     -i ${GRAPHS}/descriptions.tsv.gz --input-keys id \
+     --filter-on - --filter-keys node1
  
 echo -e "\n*** Ensure that the labels do not contain gross duplicates: sorting... ***"
 time kgtk ${KGTK_OPTIONS} sort ${GZIP_CMD} ${MGZIP_OPTIONS} \
@@ -134,6 +167,19 @@ time kgtk ${KGTK_OPTIONS} deduplicate ${MGZIP_OPTIONS} \
      -i ${WORKING_FOLDER}/labels.sorted.tsv.gz \
      -o ${GRAPHS}/labels.tsv.gz
  
+echo -e "\n*** Ensure that the labels do not contain ID duplicates. ***"
+time kgtk ${KGTK_OPTIONS} unique ${MGZIP_OPTIONS} \
+     --column id --min-count 2 --verbose \
+     -i ${GRAPHS}/labels.tsv.gz \
+     -o ${WORKING_FOLDER}/labels.duplicate-ids.tsv.gz \
+ 
+# Present a sample of the records with duplicate ID values:
+time kgtk ${KGTK_OPTIONS} head ${MGZIP_OPTIONS} -n 5 \
+     -i ${WORKING_FOLDER}/labels.duplicate-ids.tsv.gz \
+    / ifexists \
+     -i ${GRAPHS}/labels.tsv.gz --input-keys id \
+     --filter-on - --filter-keys node1
+ 
 echo -e "\n*** Ensure that the metadata do not contain gross duplicates: sorting... ***"
 time kgtk ${KGTK_OPTIONS} sort ${GZIP_CMD} ${MGZIP_OPTIONS} \
      -X "--parallel ${SORT_PARALLEL} --buffer-size ${SORT_BUFFER_SIZE} -T ${SORT_TEMP_FOLDER}" \
@@ -149,6 +195,19 @@ time kgtk ${KGTK_OPTIONS} deduplicate ${MGZIP_OPTIONS} \
      -i ${WORKING_FOLDER}/metadata.sorted.tsv.gz \
      -o ${GRAPHS}/metadata.tsv.gz
 
+echo -e "\n*** Ensure that the metadata does not contain ID duplicates. ***"
+time kgtk ${KGTK_OPTIONS} unique ${MGZIP_OPTIONS} \
+     --column id --min-count 2 --verbose \
+     -i ${GRAPHS}/metadata.tsv.gz \
+     -o ${WORKING_FOLDER}/metadata.duplicate-ids.tsv.gz \
+ 
+# Present a sample of the records with duplicate ID values:
+time kgtk ${KGTK_OPTIONS} head ${MGZIP_OPTIONS} -n 5 \
+     -i ${WORKING_FOLDER}/metadata.duplicate-ids.tsv.gz \
+    / ifexists \
+     -i ${GRAPHS}/metadata.tsv.gz --input-keys id \
+     --filter-on - --filter-keys node1
+ 
 echo -e "\n*** Ensure that the claims do not contain gross duplicates: sorting... ***"
 time kgtk ${KGTK_OPTIONS} sort ${GZIP_CMD} ${MGZIP_OPTIONS} \
      -X "--parallel ${SORT_PARALLEL} --buffer-size ${SORT_BUFFER_SIZE} -T ${SORT_TEMP_FOLDER}" \
@@ -163,6 +222,19 @@ time kgtk ${KGTK_OPTIONS} deduplicate ${MGZIP_OPTIONS} \
      -i ${WORKING_FOLDER}/claims.sorted.tsv.gz \
      -o ${GRAPHS}/claims.tsv.gz
  
+echo -e "\n*** Ensure that the claims do not contain ID duplicates. ***"
+time kgtk ${KGTK_OPTIONS} unique ${MGZIP_OPTIONS} \
+     --column id --min-count 2 --verbose \
+     -i ${GRAPHS}/claims.tsv.gz \
+     -o ${WORKING_FOLDER}/claims.duplicate-ids.tsv.gz \
+ 
+# Present a sample of the records with duplicate ID values:
+time kgtk ${KGTK_OPTIONS} head ${MGZIP_OPTIONS} -n 5 \
+     -i ${WORKING_FOLDER}/claims.duplicate-ids.tsv.gz \
+    / ifexists \
+     -i ${GRAPHS}/claims.tsv.gz --input-keys id \
+     --filter-on - --filter-keys node1
+ 
 echo -e "\n*** Ensure that the qualifiers do not contain gross duplicates: sorting... ***"
 time kgtk ${KGTK_OPTIONS} sort ${GZIP_CMD} ${MGZIP_OPTIONS} \
      -X "--parallel ${SORT_PARALLEL} --buffer-size ${SORT_BUFFER_SIZE} -T ${SORT_TEMP_FOLDER}" \
@@ -176,5 +248,18 @@ time kgtk ${KGTK_OPTIONS} deduplicate ${MGZIP_OPTIONS} \
      --columns node1 label node2 id \
      -i ${WORKING_FOLDER}/quals.sorted.tsv.gz \
      -o ${GRAPHS}/quals.tsv.gz
+ 
+echo -e "\n*** Ensure that the qualifiers do not contain ID duplicates. ***"
+time kgtk ${KGTK_OPTIONS} unique ${MGZIP_OPTIONS} \
+     --column id --min-count 2 --verbose \
+     -i ${GRAPHS}/quals.tsv.gz \
+     -o ${WORKING_FOLDER}/quals.duplicate-ids.tsv.gz \
+ 
+# Present a sample of the records with duplicate ID values:
+time kgtk ${KGTK_OPTIONS} head ${MGZIP_OPTIONS} -n 5 \
+     -i ${WORKING_FOLDER}/quals.duplicate-ids.tsv.gz \
+    / ifexists \
+     -i ${GRAPHS}/quals.tsv.gz --input-keys id \
+     --filter-on - --filter-keys node1
  
 echo -e "\n*** The data files are ready to be loaded into the graph cache. ***"
