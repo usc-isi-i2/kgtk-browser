b97eb30eff08660f946a32ae5d4d837ed2a0cd3c
g1eb <mail@g1eb.com>
2021-10-10 12:23:46 -0700

    Remove anything search related from the header to clean up the component

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,18 +1,11 @@
-import React, { useRef, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
+import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
-import InputBase from '@material-ui/core/InputBase'
-import SearchIcon from '@material-ui/icons/Search'
 import GitHubIcon from '@material-ui/icons/GitHub'
-import CircularProgress from '@material-ui/core/CircularProgress'
 
 import Logo from './Logo'
-import search from '../utils/search'
 import useStyles from '../styles/header'
 
 
@@ -20,115 +13,17 @@ const Header = ({ info }) => {
 
   const classes = useStyles()
 
-  const timeoutID = useRef(null)
-
-  const [results, setResults] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [anchorElement, setAnchorElement] = useState()
-
-  const closeMenu = () => {
-    setAnchorElement()
-  }
-
-  const handleOnChange = event => {
-    const value = event.target.value
-    clearTimeout(timeoutID.current)
-    timeoutID.current = setTimeout(() => {
-      if ( !value ) {
-        setResults([])
-        closeMenu()
-      } else if (value.length > 1)  {
-        setLoading(true)
-        search(value).then(results => {
-          if ( !!results.length ) {
-            setAnchorElement(event.target)
-            setResults(results.slice(0, 10)) // Limit the results to 10 values.
-          }
-          setLoading(false)
-        })
-      }
-    }, 500)
-  }
-
-  const handleOnKeyUp = event => {
-    const value = event.target.value
-    if (event.key === 'Enter') {
-      if ( !!results.length ) {
-        setResults([])
-        closeMenu()
-      } else if (value.length > 0) {
-        setResults([])
-        closeMenu()
-        setAnchorElement()
-      }
-    }
-  }
-
-  const renderSearchResults = () => {
-    return (
-      <Menu
-        keepMounted
-        id="search-results"
-        className={classes.menu}
-        anchorEl={anchorElement}
-        autoFocus={false}
-        disableAutoFocus={true}
-        disableEnforceFocus={true}
-        transformOrigin={{
-          vertical: -55,
-          horizontal: 0,
-        }}
-        open={!!anchorElement}
-        onClose={closeMenu}>
-        {results.map(item => (
-          <MenuItem key={item.ref}
-            onClick={closeMenu}
-            component={Link}
-            to={`/browser/${item.ref}`}
-            className={classes.menuItem}>
-            <Typography variant="body1">
-              <b>{item.ref}</b>
-              <br/>
-              {item.description}
-            </Typography>
-          </MenuItem>
-        ))}
-      </Menu>
-    )
-  }
-
   return (
     <div className={classes.grow}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <div className={classes.menuIcon}>
+          <div className={classes.logo}>
             <Logo/>
           </div>
           <Typography className={classes.title} variant="h6" noWrap>
             KGTK Browser{info ? `: ${info.graph_id}` : ''}
           </Typography>
           <div className={classes.grow} />
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={handleOnChange}
-              onKeyUp={handleOnKeyUp}
-            />
-            {loading && (
-              <div className={classes.loadingIcon}>
-                <CircularProgress size={16} />
-              </div>
-            )}
-            {renderSearchResults()}
-          </div>
           <div className={classes.sectionDesktop}>
             <IconButton
               color="inherit"
