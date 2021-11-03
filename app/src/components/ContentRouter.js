8221d9eeede606ccb24a50c3859511bac3384601
g1eb <mail@g1eb.com>
2021-11-03 15:27:35 -0700

    Fix whitespace for consistency with other files

--- app/src/components/ContentRouter.js
+++ app/src/components/ContentRouter.js
@@ -10,13 +10,13 @@ import ItemContent from './ItemContent'
 
 
 const ContentRouter = () => (
-	<BrowserRouter>
-	  <Switch>
-	    <Route exact path='/' component={Content}/>
-	    <Route exact path='/browser' component={Content}/>
-	    <Route path='/browser/:id' component={ItemContent}/>
-	  </Switch>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Content} />
+      <Route exact path='/browser' component={Content} />
+      <Route path='/browser/:id' component={ItemContent} />
+    </Switch>
+  </BrowserRouter>
 )
 
 
