48b96363804d3a36deb10933e527bae173f45069
g1eb <mail@g1eb.com>
2021-11-03 15:28:53 -0700

    Add routes prefixed with the frontend url environment variable

--- app/src/components/ContentRouter.js
+++ app/src/components/ContentRouter.js
@@ -14,7 +14,9 @@ const ContentRouter = () => (
     <Switch>
       <Route exact path='/' component={Content} />
       <Route exact path='/browser' component={Content} />
+      <Route exact path={process.env.REACT_APP_FRONTEND_URL} component={Content} />
       <Route path='/browser/:id' component={ItemContent} />
+      <Route path={`${process.env.REACT_APP_FRONTEND_URL}/:id`} component={ItemContent} />
     </Switch>
   </BrowserRouter>
 )
