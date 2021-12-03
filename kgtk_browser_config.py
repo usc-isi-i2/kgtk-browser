b813f45c0cf2b65d519a8b1017ddffbef323466f
saggu <amandeep.s.saggu@gmail.com>
2021-12-03 13:38:08 -0800

    delete build files from github

--- kgtk_browser_config.py
+++ kgtk_browser_config.py
@@ -6,8 +6,8 @@ import kgtk.kypher.api as kapi
 
 VERSION = '0.0.1'
 GRAPH_ID = 'my-knowledge-graph'
-GRAPH_CACHE = './wikidata.sqlite3.db'
-# GRAPH_CACHE = '/Volumes/saggu-ssd/wikidata-dwd-v2/kgtk-search-6/temp.kgtk-search-6/wikidata.sqlite3.db'
+# GRAPH_CACHE = './wikidata.sqlite3.db'
+GRAPH_CACHE = '/Volumes/saggu-ssd/wikidata-dwd-v2/kgtk-search-6/temp.kgtk-search-6/wikidata.sqlite3.db'
 LOG_LEVEL = 1
 INDEX_MODE = 'auto'
 MAX_RESULTS = 10000
