0f08474bb0b184f83ae856f313c762dc4af2889d
g1eb <mail@g1eb.com>
2022-01-11 15:51:22 -0800

    Render a loading indicator in the graph component while fetching data

--- app/src/components/ClassGraphViz.js
+++ app/src/components/ClassGraphViz.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-
+import useStyles from '../styles/data'
 import fetchClassGraphData from '../utils/fetchClassGraphData'
 
 
 const ClassGraphViz = () => {
 
   const { id } = useParams()
+
+  const classes = useStyles()
 
   const [data, setData] = useState({})
   const [loading, setLoading] = useState()
@@ -21,8 +24,19 @@ const ClassGraphViz = () => {
     })
   }, [])
 
+  const renderLoading = () => {
+    if ( !loading ) { return }
+    return (
+      <CircularProgress
+        size={50}
+        color="inherit"
+        className={classes.loading} />
+    )
+  }
+
   return (
     <Grid container spacing={1}>
+      {renderLoading()}
     </Grid>
   )
 }
