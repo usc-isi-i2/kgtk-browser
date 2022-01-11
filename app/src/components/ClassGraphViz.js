3992c14ea63f8aa4f99cc0b612b70c1f0fed5a26
g1eb <mail@g1eb.com>
2022-01-11 15:47:40 -0800

    Get node id and fetch the associated class graph data in the viz component

--- app/src/components/ClassGraphViz.js
+++ app/src/components/ClassGraphViz.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 
 
+import fetchClassGraphData from '../utils/fetchClassGraphData'
+
+
 const ClassGraphViz = () => {
+
+  const { id } = useParams()
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    fetchClassGraphData(id).then(data => {
+      setData(data)
+    })
+  }, [])
 
   return (
     <Grid container spacing={1}>
