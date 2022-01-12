4bb61fd64286ef772c8ac1cb2632c3561c6da938
g1eb <mail@g1eb.com>
2022-01-11 17:58:48 -0800

    Render the graph with the data from the get_class_graph_data endpoint

--- app/src/components/ClassGraphViz.js
+++ app/src/components/ClassGraphViz.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import ForceGraph2D from 'react-force-graph-2d'
 
 import useStyles from '../styles/data'
 import fetchClassGraphData from '../utils/fetchClassGraphData'
@@ -24,6 +25,41 @@ const ClassGraphViz = () => {
     })
   }, [])
 
+  const renderGraph = () => {
+    if ( !data ) { return }
+    return (
+      <ForceGraph2D
+        graphData={data}
+        nodeId={'id'}
+        nodeLabel={'tooltip'}
+        nodeVal={'size'}
+        linkWidth={link => link.width}
+        linkDirectionalArrowLength={6}
+        linkDirectionalArrowRelPos={1}
+        nodeCanvasObject={(node, ctx, globalScale) => {
+          const label = node.label;
+          const fontSize = 12/globalScale;
+          ctx.font = `${fontSize}px Sans-Serif`;
+          const textWidth = ctx.measureText(label).width;
+          const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+          ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+          ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y- 10- bckgDimensions[1] / 2, ...bckgDimensions);
+
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+
+          ctx.fillStyle = d3.schemeCategory10[node.color];
+
+          ctx.fillText(label, node.x, node.y-10);
+          ctx.beginPath(); ctx.arc(node.x, node.y, node.size, 0, 2 * Math.PI, false);  ctx.fill();
+
+          node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+        }}
+      />
+    )
+  }
+
   const renderLoading = () => {
     if ( !loading ) { return }
     return (
@@ -37,6 +73,7 @@ const ClassGraphViz = () => {
   return (
     <Grid container spacing={1}>
       {renderLoading()}
+      {renderGraph()}
     </Grid>
   )
 }
