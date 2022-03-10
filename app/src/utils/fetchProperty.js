e5d0f6ec915388607a4dffb6fa553348653b34c7
g1eb <mail@g1eb.com>
2022-03-09 17:39:22 -0800

    Add a utility function used to fetch property values

---
+++ app/src/utils/fetchProperty.js
@@ -0,0 +1,16 @@
+const fetchProperty = (id, property, skip=0, limit=10) => {
+
+  let url = `/kb/property?id=${id}&property=${property}&skip=0&limit=10`
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {method: 'GET'})
+    .then(response => response.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err))
+  })
+}
+
+export default fetchProperty
