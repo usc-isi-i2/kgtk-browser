4e71a0f9323cc86ad557f82cad960629705884ea
g1eb <mail@g1eb.com>
2022-03-10 16:49:36 -0800

    Add utility function used to fetch reverse data/ items

---
+++ app/src/utils/fetchReverseData.js
@@ -0,0 +1,16 @@
+const fetchReverseData = id => {
+
+  let url = `/kb/ritem?id=${id}`
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {method: 'GET'})
+    .then(response => response.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err))
+  })
+}
+
+export default fetchReverseData
