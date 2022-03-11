8d271fc6ce5661a231cdb3a4a2d63870a31a1db5
g1eb <mail@g1eb.com>
2022-03-10 16:51:11 -0800

    Add utility function used to fetch reverse property data

---
+++ app/src/utils/fetchReverseProperty.js
@@ -0,0 +1,17 @@
+const fetchReverseProperty = (id, property, skip=0, limit=10) => {
+
+  let url =
+    `/kb/rproperty?id=${id}&property=${property}&skip=${skip}&limit=${limit}`
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {method: 'GET'})
+    .then(response => response.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err))
+  })
+}
+
+export default fetchReverseProperty
