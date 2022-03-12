af863b95241ebcf7882d7d81be28575b677d90f5
g1eb <mail@g1eb.com>
2022-03-11 16:37:19 -0800

    Hide the link to load the first page when the property values are fetched

--- app/src/components/Data.js
+++ app/src/components/Data.js
@@ -493,11 +493,15 @@ const Data = ({ info }) => {
                   <ArrowRightAltIcon className={classes.arrow} />
                 </Grid>
                 <Grid item xs={8}>
-                  <Link className={classNames(classes.link, {item: true})}
-                    onClick={() => handleLoadRelatedValues(property)}
-                    title={`show ${property.count} values`}>
-                    show {property.count} values
-                  </Link>
+                  { !relatedPropertyValues[property.ref] ? (
+                    <Link className={classNames(classes.link, {item: true})}
+                      onClick={() => handleLoadRelatedValues(property)}
+                      title={`show ${property.count} values`}>
+                      show {property.count} values
+                    </Link>
+                  ) : (
+                    null
+                  )}
                 </Grid>
               </Grid>
             ))}
