8e9f88a58cda71857eecc3af8d33e4e4f0c254bc
g1eb <mail@g1eb.com>
2022-09-30 10:57:40 -0700

    Add the graph cache to the /kb/info endpoint for verification purposes

--- kgtk_browser_app.py
+++ kgtk_browser_app.py
@@ -169,6 +169,7 @@ def get_info():
     """
     info = {
         'graph_id': app.config.get('GRAPH_ID'),
+        'graph_cache': app.config.get('GRAPH_CACHE'),
         'version': app.config.get('VERSION'),
         'hasClassGraphVisualization': True,
         'hasIdentifiers': True,
