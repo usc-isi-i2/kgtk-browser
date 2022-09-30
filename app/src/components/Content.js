04416d8b2a799ae78900a59800f3b3423e2f33b7
g1eb <mail@g1eb.com>
2022-09-30 11:45:23 -0700

    Don't render any data in the generic content container

--- app/src/components/Content.js
+++ app/src/components/Content.js
@@ -3,11 +3,9 @@ import Container from '@material-ui/core/Container'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { makeStyles } from '@material-ui/core/styles'
 
-import Data from './Data'
 import Header from './Header'
 import ArrowUp from './ArrowUp'
 import fetchInfo from '../utils/fetchInfo'
-import fetchData from '../utils/fetchData'
 
 
 const useStyles = makeStyles(theme => ({
@@ -25,26 +23,7 @@ const Content = () => {
   const classes = useStyles()
 
   const [info, setInfo] = useState()
-  const [data, setData] = useState()
   const [loading, setLoading] = useState()
-
-  useEffect(() => {
-    // get the project configuration information
-    fetchInfo().then(info => setInfo(info))
-
-    const locationQuery = new URLSearchParams(window.location.search)
-    if ( locationQuery.has('id') ) {
-      getData(locationQuery.get('id'))
-    }
-  }, [])
-
-  const getData = id => {
-    setLoading(true)
-    fetchData(id).then(data => {
-      setLoading(false)
-      setData(data)
-    })
-  }
 
   const renderLoading = () => {
     if ( !loading ) { return }
@@ -59,9 +38,8 @@ const Content = () => {
   return (
     <React.Fragment>
       <div id="top" />
-      <Header getData={getData} info={info} />
+      <Header info={info} />
       <Container maxWidth="xl" loading={loading}>
-        {!!data && <Data info={info} data={data} />}
         {renderLoading()}
         <ArrowUp/>
       </Container>
